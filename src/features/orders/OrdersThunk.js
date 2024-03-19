579c04914bf6fbf2c4bb096dd7e7c8cd087e6e4b
Danhyeye <trancongdanhyeye@gmail.com>
2024-03-19 13:59:34 +0700

    add warn in register

--- src/features/orders/OrdersThunk.js
+++ src/features/orders/OrdersThunk.js
@@ -1,6 +1,5 @@
-
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import {OrdersService} from "../../services/OrdersService";
+import { OrdersService } from "../../services/OrdersService";
 
 export const OrdersThunk = {
     getAllOrders: createAsyncThunk("orders/get-all", async (userId) => {
